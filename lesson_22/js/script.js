3b56469729b9e2b64d49d3f6916b7d586fa2c8e9
Petro Boreiko <91476426+petro-boreiko@users.noreply.github.com>
2024-01-25 20:22:26 +0100

    Add files via upload

--- lesson_22/js/script.js
+++ lesson_22/js/script.js
@@ -5,13 +5,64 @@ icon.addEventListener("click", function () {
    document.documentElement.classList.toggle("menu-open");
 });
 
-const buttonOurPorject = document.querySelector(".our-project__button");
-const elementOurProject = document.querySelector(".our-project");
-const htmlElement = document.documentElement;
-const widthWindow = htmlElement.clientWidth;
+window.addEventListener("resize", function () {
+   transferElement(
+      ".our-project__button", // element
+      ".our-project__container", // originalParent
+      ".our-project", // newParent
+      767.98, // sizeToTransfer
+      "beforeend", // placeOriginalParent
+      "beforeend" //placeNewParent
+   );
+});
 
-   if (widthWindow < 767) {
-      elementOurProject.insertAdjacentElement("beforeend", buttonOurPorject);
-      buttonOurPorject.style.marginLeft = `15px`;
+// Функція для переносу елементу
+function transferElement(
+   element, // Наш елемент
+   originalParent, // Батько елемента
+   newParent, // Новий батько елемента
+   sizeToTransfer, // Розмір вюпорта де відбуваються зміни
+   placeOriginalParent, // Місце в функції "insertAdjacentElement"
+   placeNewParent // Місце в функції "insertAdjacentElement"
+) {
+   // Наш елемент
+   const elementDoc = document.querySelector(element);
+   // Батько елемента
+   const originalParentEl = document.querySelector(originalParent);
+   // Новий батько елемента
+   const newParentEl = document.querySelector(newParent);
+   const htmlElement = document.documentElement;
+   // Ширина вікна вюпорта
+   const widthWindow = htmlElement.clientWidth;
+
+   // Виконується перевіркаю коли має відбутися перестановка
+   if (widthWindow < sizeToTransfer) {
+      // Якщо наш ел не має класу"done" то...
+      if (!elementDoc.classList.contains("done")) {
+         // то наш елемент переноситься в нове місце
+         newParentEl.insertAdjacentElement(placeNewParent, elementDoc);
+         // І добавляємо класс до HTML для стилізації
+         htmlElement.classList.add("transfer-element");
+      }
+   } else {
+      //  Якщо HTML має даний клас то...
+      if (htmlElement.classList.contains("transfer-element")) {
+         // Наш елемент переносимо на старе місце
+         originalParentEl.insertAdjacentElement(
+            placeOriginalParent,
+            elementDoc
+         );
+         // І удаляємо клас з HTML
+         htmlElement.classList.remove("transfer-element");
+      }
    }
+}
 
+ transferElement(
+    ".our-project__button", // element
+    ".our-project__container", // originalParent
+    ".our-project", // newParent
+    767.98, // sizeToTransfer
+    "beforeend", // placeOriginalParent
+    "beforeend" //placeNewParent
+ );
